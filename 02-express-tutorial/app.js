9d838ba1a874bc701fdf0d84700068ae3065c61e
Tung Pham <tungphamwd@gmail.com>
2023-10-18 23:21:54 -0400

    assignment for week 3

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,46 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+const { products } = require("./data");
+
+app.listen(3000, () => {
+  console.log("server is listening on port 3000");
+});
+
+app.get("/api/v1/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/v1/products/:productID", (req, res) => {
+  const idToFind = parseInt(req.params.productID);
+  const product = products.find((p) => p.id === idToFind);
+  if (!product) {
+    res.status(404).json({ message: "That product was not found." });
+  } else {
+    res.json(product);
+  }
+});
+
+app.get("/api/v1/query", (req, res) => {
+  const { search, limit } = req.query;
+
+  let filteredProducts = products;
+  if (search) {
+    filteredProducts = products.filter((product) =>
+      product.name.startsWith(search)
+    );
+  }
+
+  if (limit) {
+    filteredProducts = filteredProducts.slice(0, parseInt(limit));
+  }
+
+  if (maxPrice) {
+    filteredProducts = filteredProducts.filter(
+      (product) => product.price <= parseFloat(maxPrice)
+    );
+  }
+
+  res.json(filteredProducts);
+});
+
+app.use(express.static("./public"));
